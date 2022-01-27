122440541fac99ed30bf6a78df20503a3c50991d
nathrasher1 <nathrasher1@gmail.com>
2022-01-27 09:23:18 -0500

    lesson 6

---
+++ app/src/main/java/app/swoosh/BaseActivity.kt
@@ -0,0 +1,10 @@
+package app.swoosh
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class BaseActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+}
